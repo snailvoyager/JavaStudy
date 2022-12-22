42cf1f657f87944228cab675d4dc48170ba754c8
snailvoyager <kyj88514@gmail.com>
2022-12-23 00:13:04 +0900

    EnumMap 활용

--- JavaStudy/src/basic/enumtest/CarTypeMain.java
+++ JavaStudy/src/basic/enumtest/CarTypeMain.java
@@ -1,7 +1,7 @@
 package basic.enumtest;
 
-import java.util.Arrays;
-import java.util.EnumSet;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class CarTypeMain {
     public static void main(String[] args) {
@@ -26,5 +26,23 @@ public class CarTypeMain {
         EnumSet<CarType> set3 = EnumSet.complementOf(set2);     //원하는 상수 제외
         set3.forEach(System.out::println);
         System.out.println(set3.contains(CarType.VAN));
+
+        Map<CarType, Integer> enumMap = new EnumMap<CarType, Integer>(CarType.class);
+        enumMap.put(CarType.EV, 4000);
+        //enumMap.put(null, 2);       //NPE!
+        enumMap.put(CarType.VAN, 5000);
+        enumMap.put(CarType.SUV, 4500);
+
+        for (Map.Entry<CarType, Integer> carTypeIntegerEntry : enumMap.entrySet()) {    //enum ordinal 순서 보장
+            System.out.println(carTypeIntegerEntry.getKey() + " " + carTypeIntegerEntry.getValue());
+        }
+
+        List<Map.Entry<CarType, Integer>> entries = enumMap.entrySet().stream()
+                .sorted((o1, o2) -> o2.getKey().getSeat() - o1.getKey().getSeat())    //seat 수로 내림차순 정렬
+                .collect(Collectors.toList());
+
+        for (Map.Entry<CarType, Integer> entry : entries) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
     }
 }
