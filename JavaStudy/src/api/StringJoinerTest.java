585a4572cb4b2a32601df6349c6397e107220953
snailvoyager <kyj88514@gmail.com>
2023-01-28 23:51:55 +0900

    StringJoiner

---
+++ JavaStudy/src/api/StringJoinerTest.java
@@ -0,0 +1,31 @@
+package api;
+
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.StringJoiner;
+
+public class StringJoinerTest {
+    public static void main(String[] args) {
+        List<String> titles = List.of("[상품1] 키즈 장난감", "[상품2] 중학생 책가방", "[상품3] 중학생 노트북");
+
+        LinkedHashSet<String> set = new LinkedHashSet<>();
+        titles.forEach(x -> set.addAll(List.of(x.split(" "))));
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : set) {
+            sb.append(s).append(",");
+        }
+        sb.deleteCharAt(sb.length()-1);
+        System.out.println(sb.toString());
+
+        //StringJoiner
+        StringJoiner sj = new StringJoiner(",");
+        for (String s : set) {
+            sj.add(s);
+        }
+        System.out.println(sj);
+
+        //String.join
+        System.out.println(String.join(",", set));
+    }
+}
