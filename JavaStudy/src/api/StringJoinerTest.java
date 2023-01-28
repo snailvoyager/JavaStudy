ee09cdde086682c3d41fec000b71ec47efd5cbf3
snailvoyager <kyj88514@gmail.com>
2023-01-29 00:40:10 +0900

    Arrays.asList -> Set.of

--- JavaStudy/src/api/StringJoinerTest.java
+++ JavaStudy/src/api/StringJoinerTest.java
@@ -1,16 +1,13 @@
 package api;
 
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.StringJoiner;
+import java.util.*;
 
 public class StringJoinerTest {
     public static void main(String[] args) {
-        List<String> titles = List.of("[상품1] 키즈 장난감", "[상품2] 중학생 책가방", "[상품3] 중학생 노트북");
+        List<String> titles = List.of("[상품1] 키즈 장난감", "[상품2] 중학생 책가방", "[상품3] 중학생  노트북");
 
         LinkedHashSet<String> set = new LinkedHashSet<>();
-        titles.forEach(x -> set.addAll(Arrays.asList(x.split(" "))));   //List.of -> Arrays.asList split은 null을 허용하기 때문에
+        titles.forEach(x -> set.addAll(Set.of(x.split(" "))));   //Arrays.asList -> Set.of (불변) split은 null이 아니라 공백 포함.
 
         StringBuilder sb = new StringBuilder();
         for (String s : set) {
